754148eb50593e1c4119cec640383a6cdde52411
vershinin.da <vershinin.da@dvfu.ru>
2025-07-23 22:59:12 +1000

    Password recovery functional

--- springbackend/src/main/java/ru/happines/springbackend/model/PasswordRecoveryToken.java
+++ springbackend/src/main/java/ru/happines/springbackend/model/PasswordRecoveryToken.java
@@ -2,6 +2,7 @@ package ru.happines.springbackend.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Date;
@@ -9,6 +10,7 @@ import java.util.Date;
 @Entity
 @Table(name = "password_recovery_tokens")
 @Data
+@NoArgsConstructor
 public class PasswordRecoveryToken {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
